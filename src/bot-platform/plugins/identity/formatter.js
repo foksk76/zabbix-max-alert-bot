397919e87ec14700a02b2421d837a52dfea7ee4a
foksk76 <foksk76@gmail.com>
2026-07-08 08:33:12 +0700

    Implement identity response formatter

---
+++ src/bot-platform/plugins/identity/formatter.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const RECIPIENT_TYPE_BY_KIND = Object.freeze({
+  user: 'user_id',
+  chat: 'chat_id'
+});
+
+function formatIdentityResponse(event) {
+  const recipient = event && event.recipient ? event.recipient : {};
+  const recipientType = RECIPIENT_TYPE_BY_KIND[recipient.kind];
+
+  if (!recipientType) {
+    throw new Error('Unsupported identity recipient kind');
+  }
+
+  if (!recipient.value) {
+    throw new Error('Missing identity recipient value');
+  }
+
+  return {
+    kind: 'identity',
+    recipient: {
+      kind: recipient.kind
+    },
+    zabbix: {
+      recipientType,
+      to: recipient.value
+    },
+    text: formatText(recipientType, recipient.value)
+  };
+}
+
+function formatText(recipientType, to) {
+  return [
+    'Use these Zabbix recipient parameters:',
+    `RecipientType: ${recipientType}`,
+    `To: ${to}`
+  ].join('\n');
+}
+
+module.exports = {
+  formatIdentityResponse
+};
